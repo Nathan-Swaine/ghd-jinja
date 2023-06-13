dfd993764dad01ca903248cb6451569ba7c3bfab
Nathan-Swaine <nathanswaine18@gmail.com>
2023-06-13 14:58:14 +0100

    basic template

--- hello.py
+++ hello.py
@@ -4,4 +4,144 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return ""
+    return """
+       <table class="sm-w-full" cellpadding="0" cellspacing="0" role="presentation" style="width: 800px; background: #000000;">
+            <tr>
+                <td cellpadding="0" width="100%" cellspacing="0" dir="ltr" valign="top" align="center" bgcolor="#000000" class=" sm-w-full inline-block  pb-20 " style="background-color: #000000; padding-bottom:30px; ">
+                    <a href="https://www.ghdhair.com/curling-wands-and-tongs-c"> <img src="http://c94df97feba3ed270b40-dccdaa2c75856b59b01c3723d5ef3b50.r0.cf3.rackcdn.com/promo_2023/15-curve/uk-1.jpg" alt="" valign="top" width="800" height="518" border="0" class="always-full-width" style="width:100%;height:auto;min-width:auto;display: block; min-width: 800px; width: 800px;"></a>
+                </td>
+            </tr>
+            <tr style="background: #000000;">
+                <td align="center" class="" style=" padding-bottom: 50px;">
+                    <a href="https://www.ghdhair.com/curling-wands-and-tongs-c"><img src="http://c94df97feba3ed270b40-dccdaa2c75856b59b01c3723d5ef3b50.r0.cf3.rackcdn.com/promo_2023/15-curve/uk-2.jpg" alt="" valign="top" width="800" height="165" border="0" class="always-full-width" style="width:100%;height:auto;min-width:auto;display: block; min-width: 800px; width: 800px;"></a>
+                </td>
+            </tr>
+            <tr>
+                <td valign="top" align="center" style="background: #000000;">
+                    <table width="90%" align="center" cellpadding="0" cellspacing="0" role="presentation">
+                        <tr>
+                            <th valign="top" bgcolor="#000000" class=" body-text px-3  " align="center" style="
+                                font-family: Helvetica, Arial, sans-serif;
+                                font-size: 18px;
+                                font-weight: lighter;
+                                text-align: center;
+                                line-height: 19px !important;
+                                letter-spacing: 0px;
+                                color: #ffffff;
+                                padding-left: 30px;
+                                padding-right: 30px;
+                                padding-bottom: 30px;">
+                                From hair tongs to stylers and hair dryers, we&rsquo;ve got the styling tool to achieve your desired look. Hurry! Take 15% off* for a limited time only on selected styling heated tools. <br><br><span style=" font-weight: 700; font-size: 20px;">USE CODE: <span style="color: #d6af63;">GHD 15</span></span>
+                            </th>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td align="center" valign="top" bgcolor="#000000" style="border-collapse:collapse;padding:0;margin:0; ">
+                    <table valign="center" align="center" bgcolor="#000000" cellpadding="0" cellspacing="0">
+                        <tr>
+                            <td class="pr-30 pl-30  cta-float-none  pl-0 pr-0 " cellpadding="0" cellspacing="0" border="0" valign="bottom" align="center" bgcolor="#000000" style="background-color: #000000; padding-left: 25px; padding-right: 25px; text-align: center; font-weight: bold; padding-bottom: 100px;">
+                                <table valign="center" align="center" bgcolor="#000000" cellpadding="0" cellspacing="0">
+                                    <tr>
+                                        <td align="center" width="200" strokecolor="000000" style="
+                                                width:200;
+                                                padding-top:10px;
+                                                padding-bottom:10px;
+                                                padding-left:10px;
+                                                padding-right:10px;
+                                                font-family: Helvetica, Arial, sans-serif;
+                                                font-size:16px;
+                                                line-height: 25px !important;
+                                                vertical-align: middle;
+                                                background-color: #f8f8f8;
+                                                text-align: center;">
+                                                    <a strokecolor="#000000" href="https://www.ghdhair.com/curling-wands-and-tongs-c" style="
+                                                        display: inline-block;
+                                                        color: #000000 !important; 
+                                                        background-color: #f8f8f8;
+                                                        text-decoration: none;
+                                                        font-size: 16px;
+                                                        font-weight: bolder;
+                                                        border-color: #ffffff;">
+                                                        <font style="color: rgb(0,0,0);">
+                                                            SHOP NOW                                                </font>
+                                                    </a>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td cellpadding="0" width="100%" cellspacing="0" dir="ltr" valign="top" align="center" bgcolor="#000000" class=" sm-w-full inline-block  pb-20 " style="background-color: #000000; padding-bottom:30px; ">
+                    <a href="https://www.ghdhair.com/personalise-c"> <img src="http://c94df97feba3ed270b40-dccdaa2c75856b59b01c3723d5ef3b50.r0.cf3.rackcdn.com/promo_2023/15-curve/uk-3.jpg" alt="" valign="top" width="800" height="513" border="0" class="always-full-width" style="width:100%;height:auto;min-width:auto;display: block; min-width: 800px; width: 800px;"></a>
+                </td>
+            </tr>
+            <tr style="background: #000000;">
+                <td align="center" class="" style=" padding-bottom: 50px;">
+                    <a href="https://www.ghdhair.com/personalise-c"><img src="http://c94df97feba3ed270b40-dccdaa2c75856b59b01c3723d5ef3b50.r0.cf3.rackcdn.com/promo_2023/15-curve/uk-4.jpg" alt="" valign="top" width="800" height="78" border="0" class="always-full-width" style="width:100%;height:auto;min-width:auto;display: block; min-width: 800px; width: 800px;"></a>
+                </td>
+            </tr>
+            <tr>
+                <td valign="top" align="center" style="background: #000000;">
+                    <table width="90%" align="center" cellpadding="0" cellspacing="0" role="presentation">
+                        <tr>
+                            <th valign="top" bgcolor="#000000" class=" body-text px-3  " align="center" style="
+                                font-family: Helvetica, Arial, sans-serif;
+                                font-size: 18px;
+                                font-weight: lighter;
+                                text-align: center;
+                                line-height: 19px !important;
+                                letter-spacing: 0px;
+                                color: #ffffff;
+                                padding-left: 30px;
+                                padding-right: 30px;
+                                padding-bottom: 30px;">
+                                Your hair is unique, so why not make your mark by personalising your new tool? Engrave your name or words that mean something to you on a variety of selected heated styling tools**! The perfect gift for yourself or your loved ones.
+                            </th>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td align="center" valign="top" bgcolor="#000000" style="border-collapse:collapse;padding:0;margin:0; ">
+                    <table valign="center" align="center" bgcolor="#000000" cellpadding="0" cellspacing="0">
+                        <tr>
+                            <td class="pr-30 pl-30  cta-float-none  pl-0 pr-0 " cellpadding="0" cellspacing="0" border="0" valign="bottom" align="center" bgcolor="#000000" style="background-color: #000000; padding-left: 25px; padding-right: 25px; text-align: center; font-weight: bold; padding-bottom: 100px;">
+                                <table valign="center" align="center" bgcolor="#000000" cellpadding="0" cellspacing="0">
+                                    <tr>
+                                        <td align="center" width="200" strokecolor="000000" style="
+                                            width:200;
+                                            padding-top:10px;
+                                            padding-bottom:10px;
+                                            padding-left:10px;
+                                            padding-right:10px;
+                                            font-family: Helvetica, Arial, sans-serif;
+                                            font-size:16px;
+                                            line-height: 25px !important;
+                                            vertical-align: middle;
+                                            background-color: #f8f8f8;
+                                            text-align: center;">
+                                                <a strokecolor="#000000" href="https://www.ghdhair.com/personalise-c" style="
+                                                    display: inline-block;
+                                                    color: #000000 !important; 
+                                                    background-color: #f8f8f8;
+                                                    text-decoration: none;
+                                                    font-size: 16px;
+                                                    font-weight: bolder;
+                                                    border-color: #ffffff;">
+                                                    <font style="color: rgb(0,0,0);">
+                                                        SHOP NOW                                                </font>
+                                                </a>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>"""
---
+++ readme.txt
@@ -0,0 +1,10 @@
+https://flask.palletsprojects.com/en/2.3.x/quickstart/
+
+install flask
+ 
+update pip to >2.7
+
+flask auto isntall jinja and some other useful stuff
+
+cd ghd-jinja
+flas --app hello run
